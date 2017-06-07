168da589183838bad3027e20e756028b38bd3709
Niklas Selke <n.selke@fz-juelich.de>
2017-06-07 15:48:17 +0200

    added client package

---
+++ src/client/Client.java
@@ -0,0 +1,26 @@
+package client;
+
+import generated.AwaitMoveMessageType;
+import generated.MoveMessageType;
+
+public class Client {
+	public Client(String ip, int port, String name) {
+		
+	}
+	
+	public int login() {
+		return 0;
+	}
+	
+	public AwaitMoveMessageType awaitMove() {
+		return null;
+	}
+	
+	public void sendMove(MoveMessageType move) {
+		
+	}
+	
+	public boolean gameOver() {
+		return true;
+	}
+}
