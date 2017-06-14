c5e24bab16c9856ac9a809e090b3721f05a9bc14
cxan96 <c.p.xanten@gmail.com>
2017-06-14 07:37:56 +0200

    wait for server response

--- src/MazeNet.java
+++ src/MazeNet.java
@@ -1,11 +1,21 @@
 import client.Client;
 
+import java.util.Scanner;
+
 public class MazeNet {
     public static void main (String args []) {
-	//TODO gegebenenfalls ip und port anpassen
-	Client client = new Client("localhost", 5123, "Abel");
+	try {
+	    //TODO gegebenenfalls ip und port anpassen
+	    Client client = new Client("localhost", 5123, "Abel");
 
-	int id = client.login();
-	System.out.println("ID = " + id);
+	    int id = client.login();
+	    System.out.println("ID = " + id);
+
+	    // wait to make it possible for the server to respond
+	    Scanner sc = new Scanner(System.in);
+	    sc.nextLine();
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
     }
 }
