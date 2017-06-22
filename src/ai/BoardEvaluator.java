d69fe18d187cbc1c29fa60d29fab380a8645edb9
cxan96 <c.p.xanten@gmail.com>
2017-06-22 08:01:07 +0200

    added server classes for representation and updated uml diagram

--- src/ai/BoardEvaluator.java
+++ src/ai/BoardEvaluator.java
@@ -3,5 +3,5 @@ package ai;
 import generated.*;
 
 public interface BoardEvaluator {
-    public double evaluate(AbelBoard board, int playerID, TreasureType treasure);
+    public double evaluate(Board board, int playerID, TreasureType treasure);
 }
