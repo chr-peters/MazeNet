fb36ab5bf30878ed7cab3660d95aa006174dc189
cxan96 <c.p.xanten@gmail.com>
2017-06-29 07:44:08 +0200

    added basic structure of GameSimulator.java

---
+++ src/ai/GameSimulator.java
@@ -0,0 +1,33 @@
+package ai;
+
+import generated.*;
+
+import java.util.Map;
+
+public class GameSimulator {
+
+    /**
+     * This method simulates a game on a random board.
+     *
+     * @param players Each AI mapped to its playerID
+     *
+     * @return The ID of the winning player
+     */
+    public int simulate(Map<Integer, AI> players) {
+	return 0;
+    }
+
+    /**
+     * This method simulates a game with specified parameters.
+     *
+     * @param players          Each AI mapped to its playerID
+     * @param board            The starting board for the simulation
+     * @param currentTreasures The current treasure for each playerID
+     *
+     * @return The ID of the winning player
+     */
+    public int simulate(Map<Integer, AI> players, Board board, Map<Integer, TreasureType> currentTreasures) {
+	return 0;
+    }
+    
+}
