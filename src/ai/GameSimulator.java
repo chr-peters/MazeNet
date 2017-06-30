558de9189e2eb73c445fc57c517e2c6a8b0254b7
cxan96 <c.p.xanten@gmail.com>
2017-06-30 18:57:07 +0200

    extended GameSimulator.java

--- src/ai/GameSimulator.java
+++ src/ai/GameSimulator.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Collections;
+import java.util.Stack;
 
 public class GameSimulator {
 
@@ -41,8 +42,13 @@ public class GameSimulator {
 	    // remove the treasure from the available list
 	    allTreasures.remove(0);
 	}
+	// each player still has to find the same amount of treasures
+	Map<Integer, Integer> treasuresToGo = new HashMap<>();
+	for (int id: players.keySet()) {
+	    treasuresToGo.put(id, 24/players.size() + 1);
+	}
 	// simulate the game
-	return simulate(players, new Board(), currentTreasures);
+	return simulate(players, new Board(), currentTreasures, Collections.emptyList(), treasuresToGo, 1);
     }
 
     /**
@@ -51,11 +57,58 @@ public class GameSimulator {
      * @param players          Each AI mapped to its playerID
      * @param board            The starting board for the simulation
      * @param currentTreasures The current treasure for each playerID
+     * @param foundTreasures   The treasures that have already been found
+     * @param nextMove         The ID of the player who's turn is next
+     * @param treasuresToGo    How many treasures does each player still have to find?
      *
      * @return The ID of the winning player
      */
-    public int simulate(Map<Integer, AI> players, Board board, Map<Integer, TreasureType> currentTreasures) {
-	return 0;
+    public int simulate(Map<Integer, AI> players, Board board, Map<Integer, 
+			TreasureType> currentTreasures, List<TreasureType> foundTreasures, 
+			Map<Integer, Integer> treasuresToGo, int nextMove) {
+	// first get all available treasures
+	List<TreasureType> availableTreasures = getAllTreasures();
+	availableTreasures.removeAll(foundTreasures);
+	availableTreasures.removeAll(currentTreasures.values());
+
+	// now create a stack for each player representing the treasures he still has to find
+	Map<Integer, Stack<TreasureType>> playerStacks = new HashMap<>();
+	
+	// now fill the stack for each player with random treasures
+	Collections.shuffle(availableTreasures);
+	for(int id: players.keySet()) {
+	    playerStacks.put(id, new Stack<>());
+	    // at the bottom of the stack, place the starting position
+	    playerStacks.get(id).push(TreasureType.fromValue("Start0"+id));
+	    // now fill the stack
+	    for (int i = 0; i<treasuresToGo.get(id)-2; i++) {
+		playerStacks.get(id).push(availableTreasures.get(0));
+		availableTreasures.remove(0);
+	    }
+	    // add the current treasure at the top
+	    playerStacks.get(id).push(currentTreasures.get(id));
+	}
+
+	// the game loop
+	while (true) {
+	    // let each player make a move
+	    for (int i=0; i<players.size(); i++) {
+		int currentID = Math.max((nextMove + i) % (players.size()+1), 1);
+		
+		// create the AwaitMoveMessageType, use copies for safety reasons
+		AwaitMoveMessageType msg = new AwaitMoveMessageType();
+		msg.setBoard(new Board(board));
+		List<TreasuresToGoType> tmpTGT = new ArrayList<>();
+		for (int id: players.keySet()) {
+		    TreasuresToGoType tmp = new TreasuresToGoType();
+		    tmp.setPlayer(id);
+		    tmp.setTreasures(playerStacks.get(id).size());
+		    tmpTGT.add(tmp);
+		}
+		// msg.setTreasuresToGo(tmpTGT);
+		// TODO extend AwaitMoveMessageType to add treasuresToGo
+	    }
+	}
     }
 
     public static List<TreasureType> getAllTreasures() {
